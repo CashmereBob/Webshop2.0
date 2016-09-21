9fdd6e559f422be2c67275fed0469f4b1f8f8924
CashmereBob <weidmar@gmail.com>
2016-09-21 15:47:16 +0200

    asd

--- WebShop_Group7/WebShop_Group7/Models/Order.cs
+++ WebShop_Group7/WebShop_Group7/Models/Order.cs
@@ -19,23 +19,34 @@ namespace WebShop_Group7.Models
 
                 using (DataTable dt = new DataTable("Order"))
                 {
-                    dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Id"), new DataColumn("UserID") });
+                    dt.Columns.AddRange(new DataColumn[3] { new DataColumn("ID"), new DataColumn("Firstname"), new DataColumn("Lastname") });
 
-                    string sql = "Select * From tbl_Order";
+                    string sql = @"Select tbl_Order.ID AS [orderID], tbl_User.Firstname AS [Firstname], tbl_User.Lastname AS [Lastname] " +
+                                    @"From[tbl_Order_Product-Attribute] " +
+                                    @"INNER JOIN tbl_Order " +
+                                    @"ON[tbl_Order_Product-Attribute].OrderID = tbl_Order.ID " +
+                                    @"INNER JOIN tbl_Product_Attribute " +
+                                    @"ON[tbl_Order_Product-Attribute].ProductAttributeID = tbl_Product_Attribute.ID " +
+                                    @"INNER JOIN tbl_User " +
+                                    @"ON tbl_Order.UserID = tbl_User.ID " +
+                                    @"INNER JOIN tbl_Carrier " +
+                                    @"ON tbl_Order.CarrierID = tbl_Carrier.ID " +
+                                    @"INNER JOIN tbl_Payment " +
+                                    @"ON tbl_Order.PaymentID = tbl_Payment.ID";
+
                     SqlCommand myCommand = new SqlCommand(sql, db._connection);
-
-                    
 
 
                     using (SqlDataReader myDataReader = myCommand.ExecuteReader())
                     {
-                        var hepp = "hej";
+
                         while (myDataReader.Read())
                         {
-                            var id = int.Parse(myDataReader["ID"].ToString());
-                            var user = myDataReader["UserID"].ToString();
+                            int orderID = int.Parse(myDataReader["orderID"].ToString());
+                            string firstname = myDataReader["Firstname"].ToString();
+                            string lastname = myDataReader["Lastname"].ToString();
 
-                            dt.Rows.Add(id, user);
+                            dt.Rows.Add(orderID, firstname, lastname);
                         }
                     }
                     myCommand.ExecuteNonQuery();
