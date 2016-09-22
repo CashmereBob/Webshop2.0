c791bdc09d6218ca70159024779b4b29a51ee7cb
lundgren84 <lundgren84@hotmail.se>
2016-09-22 19:28:33 +0200

    asd

--- WebShop_Group7/WebShop_Group7/Models/Product.cs
+++ WebShop_Group7/WebShop_Group7/Models/Product.cs
@@ -33,11 +33,6 @@ namespace WebShop_Group7.Models
                        new DataColumn("boolAttribute"),
                     });
 
-                    //                    string query = "SELECT * FROM tbl_Product";
-                    string test = $@"select tbl_Product.ID,tbl_Product.Name,tbl_Category.Name AS category,tbl_Brand.Name as theBrand,tbl_Product.Description  from tbl_Product
-                    INNER JOIN tbl_Brand ON tbl_Brand.ID = tbl_Product.BrandID
-                    INNER JOIN tbl_Category ON tbl_Category.ID = tbl_Product.CategoryID";
-
                     string test2 = $@"Select 
 tbl_Product.ID,
 tbl_Product_Attribute.ArticleNumber,
@@ -71,17 +66,6 @@ INNER JOIN tbl_Category ON tbl_Category.ID = tbl_Product.CategoryID";
                                 var customPrice =    (dataReader["b2c"].ToString()+" kr");
                                 bool attribute = (bool)dataReader["Attribute"];
                               
-                                //1new DataColumn("ID"),
-                                //2new DataColumn("ArticleNr"),
-                                //3new DataColumn("Name"),
-                                //4new DataColumn("CategoryID"),          
-                                //5new DataColumn("BrandID"),
-                                //6new DataColumn("Description"),
-                                //7new DataColumn("b2bPrice"),
-                                //8new DataColumn("b2cPrice"),
-                                //9new DataColumn("boolAttribute"),
-                             
-
                                 dataTable.Rows.Add(id, articlenr, name, categoryid, brandid,
                                 description, buissniesPrice, customPrice, attribute);
                             }
