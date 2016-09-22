5bc6a322495e33aeea9dab50cb1d4439a017844e
CashmereBob <weidmar@gmail.com>
2016-09-22 09:22:27 +0200

    Objektklasser

--- WebShop_Group7/WebShop_Group7/Models/OrderObject.cs
+++ WebShop_Group7/WebShop_Group7/Models/OrderObject.cs
@@ -7,5 +7,33 @@ namespace WebShop_Group7.Models
 {
     public class OrderObject
     {
+        public int orderId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string adress { get; set; }
+        public string postalCode { get; set; }
+        public string city { get; set; }
+
+        public string telephone { get; set; }
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public List<ProductObject> products { get; set; }
+
+        public string carrier { get; set; }
+        public string carrierService { get; set; }
+        public decimal carrierPrice { get; set; }
+
+        public string payment { get; set; }
+        public string paymentService { get; set; }
+        public decimal paymentPrice { get; set; }
+
+        public decimal sum { get; set; }
+
+
+
+
+
+
+
     }
 }
