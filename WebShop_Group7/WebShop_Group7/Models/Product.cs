b07c9c53a4a12faf1d2b928e420f43c547dff3c6
lundgren84 <lundgren84@hotmail.se>
2016-09-22 09:21:26 +0200

    GridProducts!!

--- WebShop_Group7/WebShop_Group7/Models/Product.cs
+++ WebShop_Group7/WebShop_Group7/Models/Product.cs
@@ -20,13 +20,19 @@ namespace WebShop_Group7.Models
                 connection.OpenConnection();
                 using (dataTable)
                 {
-                    dataTable.Columns.AddRange(new DataColumn[5]
-                    {   new DataColumn("ID"),
-                        new DataColumn("Name"),
-                        new DataColumn("Description"),
-                        new DataColumn("BrandID"),
-                        new DataColumn("CategoryID"),
-                      
+                    dataTable.Columns.AddRange(new DataColumn[12]
+                    {  new DataColumn("ID"),
+                       new DataColumn("ArticleNr"),
+                       new DataColumn("Name"),
+                       new DataColumn("CategoryID"),
+                       new DataColumn("AttributeType"),
+                       new DataColumn("AttributeValue"),
+                       new DataColumn("BrandID"),
+                       new DataColumn("Description"),
+                       new DataColumn("b2bPrice"),
+                       new DataColumn("b2cPrice"),
+                       new DataColumn("quant"),
+                       new DataColumn("img")
                     });
 
                     //                    string query = "SELECT * FROM tbl_Product";
@@ -36,31 +42,49 @@ namespace WebShop_Group7.Models
 
                     string test2 = $@"select 
 tbl_Product.ID,
+tbl_Product_Attribute.ArticleNumber,
 tbl_Product.Name,
-tbl_Category.Name AS Category,
-tbl_Brand.Name as Brand,
+tbl_Category.Name AS category,
+tbl_Attribute.Name AS theType,
+tbl_Attribute.Value AS value,
+tbl_Brand.Name as theBrand,
 tbl_Product.Description,
-tbl_Product_Attribute.PriceB2C as B2C  
+tbl_Product_Attribute.PriceB2B as b2b,
+tbl_Product_Attribute.PriceB2C as b2c,
+
+tbl_Product_Attribute.Quantity,
+tbl_Product.ImgUrl
+
+
+
 from tbl_Product
 
 INNER JOIN tbl_Brand ON tbl_Brand.ID = tbl_Product.BrandID
 INNER JOIN tbl_Category ON tbl_Category.ID = tbl_Product.CategoryID
-INNER JOIN tbl_Product_Attribute On tbl_Product_Attribute.ProductID = tbl_Product.ID";
+INNER JOIN tbl_Product_Attribute ON tbl_Product_Attribute.ProductID = tbl_Product.ID
+INNER JOIN tbl_Attribute ON tbl_Attribute.ID = tbl_Product_Attribute.AttributeID1";
 
-                    using (SqlCommand command = new SqlCommand(test, connection._connection))
+                    using (SqlCommand command = new SqlCommand(test2, connection._connection))
                     {
                         using (dataReader = command.ExecuteReader())
                         {
                             while (dataReader.Read())
                             {
                                 var id = int.Parse(dataReader["ID"].ToString());
-                                var categoryid = dataReader["category"].ToString();
+                                var articlenr = int.Parse(dataReader["ArticleNumber"].ToString());
                                 var name = dataReader["Name"].ToString();
-                                var description = dataReader["Description"].ToString();
+                                var categoryid = dataReader["category"].ToString();
+                                var attriType = dataReader["theType"].ToString();
+                                var attriValue = dataReader["value"].ToString();
                                 var brandid = dataReader["theBrand"].ToString();
-                              
+                                var description = dataReader["Description"].ToString();                   
+                                var buissniesPrice = double.Parse(dataReader["b2b"].ToString());
+                                var customPrice = double.Parse(dataReader["b2c"].ToString());
+                                var quant = int.Parse(dataReader["Quantity"].ToString());
+                                var imgurl = dataReader["ImgUrl"].ToString();
 
-                                dataTable.Rows.Add(id, name, description, brandid, categoryid);
+                                dataTable.Rows.Add(id, articlenr, name, categoryid, attriType, attriValue, brandid,
+                                description , buissniesPrice, customPrice, quant, imgurl);
                             }
 
                             return dataTable;
