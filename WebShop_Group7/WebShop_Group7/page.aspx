89a13dce819b4bce433e71fa406d91f1d8ae7e31
CashmereBob <weidmar@gmail.com>
2016-09-25 15:47:39 +0200

    more pages

--- WebShop_Group7/WebShop_Group7/page.aspx
+++ WebShop_Group7/WebShop_Group7/page.aspx
@@ -1,16 +1,8 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="page.aspx.cs" Inherits="WebShop_Group7.page" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="page.aspx.cs" Inherits="WebShop_Group7.page" MasterPageFile="~/Site.Master"%>
 
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     
-    </div>
-    </form>
-</body>
-</html>
+    <div id="content" runat="server"></div>
+
+</asp:Content>
+
--- WebShop_Group7/WebShop_Group7/page.aspx.cs
+++ WebShop_Group7/WebShop_Group7/page.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebShop_Group7.Models;
 
 namespace WebShop_Group7
 {
     public partial class page : System.Web.UI.Page
     {
+        Pages pageDal = new Pages();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Request.QueryString["id"] != null) { 
+            PageObject page = pageDal.GetPageById(int.Parse(Request.QueryString["id"]));
+            content.InnerHtml = page.content;
+            } else
+            {
+                Response.Redirect($"~/");
+            }
         }
     }
 }
