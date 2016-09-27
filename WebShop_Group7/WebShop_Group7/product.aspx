887d3d1ea67ba01d274bc643fe5eb24d6fca6684
CashmereBob <weidmar@gmail.com>
2016-09-27 19:12:59 +0200

    product

--- WebShop_Group7/WebShop_Group7/product.aspx
+++ WebShop_Group7/WebShop_Group7/product.aspx
@@ -1,16 +1,25 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="product.aspx.cs" Inherits="WebShop_Group7.product" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="product.aspx.cs" Inherits="WebShop_Group7.product" %>
 
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     
+    <div class="row">
+        <div id="head" class="col-xs-12" runat="server">
+
+        </div>
+
+
     </div>
-    </form>
-</body>
-</html>
+
+    <div class="row">
+        <div id="img" class="col-xs-12 col-sm-6" runat="server">
+
+        </div>
+        <div id="description" class="col-xs-12 col-sm-6" runat="server">
+
+        </div>
+        <div id="details" class="col-xs-12 col-sm-6" runat="server">
+
+        </div>
+    </div>
+
+</asp:Content>
--- WebShop_Group7/WebShop_Group7/product.aspx.cs
+++ WebShop_Group7/WebShop_Group7/product.aspx.cs
@@ -1,17 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebShop_Group7.Models;
 
 namespace WebShop_Group7
 {
     public partial class product : System.Web.UI.Page
     {
+        Users usrDal = new Users();
+        Product proDal = new Product();
+        int pricegroup = 1;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["User"] != null)
+            {
+                UserObject usr = usrDal.GetUserById((int)Session["User"]);
+                pricegroup = usr.priceGroup;
+            }
+
+            if (Request.QueryString["id"] != null)
+            {
+                if (!IsPostBack)
+                {
+                    DrawProduct(int.Parse(Request.QueryString["id"]));
+                }
+            }
+            else
+            {
+                Response.Redirect("~/category.aspx");
+            }
+
 
         }
+
+        protected void DrawProduct(int id)
+        {
+
+            string price = string.Empty;
+
+            ProductObject product = proDal.GetProduct(id);
+
+            Dictionary<string, List<string>> attributes = GetAllAtributes(id);
+
+            head.InnerHtml = $"<h2>{product.name}</h2><h3>{product.brandName}</h3><h4>{product.category}</h4>";
+            img.InnerHtml = $"<div class=\"thumbnail\"><img src = \"{product.imgURL}\" alt=\"{product.name}\" ></div>";
+
+            StringBuilder str = new StringBuilder();
+
+            str.Append($"<h3>Produktbeskrivning</h3><p>{product.description}</p>");
+
+            str.Append($"<h4 class=\"text-right\">");
+
+            if (attributes != null)
+            {
+                foreach (KeyValuePair<string, List<string>> atr in attributes)
+                {
+                    str.Append($" {atr.Key}: <select> ");
+
+                    foreach (string value in atr.Value)
+                    {
+                        str.Append($" <option value=\"{proDal.GetAttributeID(atr.Key, value)}\">{value}</option> ");
+                    }
+                    str.Append("</select>");
+                }
+            }
+
+            str.Append("</h4>");
+            
+
+            
+
+            if (pricegroup == 1) { price = product.priceB2C.ToString("#,##"); }
+            if (pricegroup == 2) { price = product.priceB2B.ToString("#,##"); }
+
+            str.Append($"<h2 class=\"text-right\" >{price}kr</h2><p class=\"text-right\" ><i>moms: {(int.Parse(price) * 0.25)}kr</i></p>");
+            str.Append($"<p class=\"text-right\"><a href =\"/product.aspx?id={id}\" class=\"btn btn-success\" role=\"button\">Lägg i varukorg</a></p>");
+
+
+
+            description.InnerHtml = str.ToString();
+        }
+
+
+        protected Dictionary<string, List<string>> GetAllAtributes(int id)
+        {
+            List<ProductObject> subProducts = proDal.GetProductByWhereList($"WHERE tbl_Product.ID = '{id}'");
+            Dictionary<string, List<string>> attributes = new Dictionary<string, List<string>>();
+
+            if (subProducts != null)
+            {
+                foreach (ProductObject sub in subProducts)
+                {
+                    Dictionary<string, string> attribute = proDal.GetAttribute(sub);
+                    if (attribute != null)
+                    {
+                        foreach (KeyValuePair<string, string> values in attribute)
+                        {
+                            if (attributes.ContainsKey(values.Key))
+                            {
+                                if (!attributes[values.Key].Contains(values.Value))
+                                {
+                                    attributes[values.Key].Add(values.Value);
+                                }
+
+
+                            }
+                            else
+                            {
+                                attributes.Add(values.Key, new List<string>() { values.Value });
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+            return attributes;
+        }
+
     }
 }
